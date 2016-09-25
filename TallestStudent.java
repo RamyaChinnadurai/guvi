291c20d309ff0f448461d9a5ce46101e77aa80d5
Ramya Chinnadurai <RamyaChinnadurai@users.noreply.github.com>
2016-09-25 21:39:23 +0530

    Create TallestStudent.java

---
+++ TallestStudent.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class TallestStudent
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int[] h = new int[6];
+		for(int i=0;i<6;i++){
+			h[i] = s.nextInt();
+		}
+		Arrays.sort(h);
+		System.out.println(h[3]);
+		int k=s.nextInt();
+		System.out.println(h[k-1]);
+		
+	}
+}
